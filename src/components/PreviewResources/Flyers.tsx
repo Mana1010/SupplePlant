8840136be6fc2fab4d0198b90c60a5a17d19fe0e
Mana1010 <tristanvicclarito2003@gmail.com>
2025-01-12 22:43:56 +0800

    adding a flyer

--- src/components/PreviewResources/Flyers.tsx
+++ src/components/PreviewResources/Flyers.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-
+import Image from "next/image";
+import flyer from "../../../public/assets/suppleplant-flyer.png";
 function Flyers() {
   return (
-    <div className="w-full flex items-center justify-center h-screen">
-      No Flyers Available right now
+    <div className="w-full flex items-center justify-center flex-col space-y-2">
+      <Image src={flyer} alt="flyer" width={300} height={300} priority />
+      <a
+        href="/assets/suppleplant-flyer.png"
+        download="suppleplant-flyer.png"
+        className="bg-secondColor text-white rounded-sm font-bold text-md py-2 px-3"
+      >
+        Get a Copy
+      </a>
     </div>
   );
 }
